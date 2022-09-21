5d8cac9466c72121978486fb7d0ba6fe15c9bc14
MariaDuda <mariabevenuto062@gmail.com>
2022-09-21 00:43:53 -0300

    [FIX] it

--- cypress/e2e/createRecommendation.cy.js
+++ cypress/e2e/createRecommendation.cy.js
@@ -2,11 +2,11 @@ import {faker} from "@faker-js/faker";
 
 
 describe('test creation of a recommendation', () => {
-  const recommendation = {
-    name: faker.random.words(),
-    link: `https://www.youtube.com/watch?v=${faker.random.alphaNumeric(11)}`
-  }
   it('passes', () => {
+    const recommendation = {
+      name: faker.random.words(),
+      link: `https://www.youtube.com/watch?v=${faker.random.alphaNumeric(11)}`
+    }
     cy.visit('http://localhost:3000/')
     cy.get('#name').type(recommendation.name);
     cy.get('#link').type(recommendation.link);
