97577df586ca1895f17ec1fbd2479f2946b16eee
MariaDuda <mariabevenuto062@gmail.com>
2022-09-24 15:45:23 -0300

    [feat] random upvote

--- cypress/e2e/random.cy.js
+++ cypress/e2e/random.cy.js
@@ -1,40 +1,53 @@
 import { faker } from "@faker-js/faker";
 
 
+before(async () => {
+  await cy.resetDatabase()
+});
 
-describe('test creation of a recommendation', () => {
-  it('create a recommendation with correct input schema, sucess', () => {
-    const recommendation = {
-      name: faker.random.words(),
-      link: `https://www.youtube.com/watch?v=${faker.random.alphaNumeric(11)}`
-    }
-    cy.visit('http://localhost:3000/')
-    cy.get('#name').type(recommendation.name);
-    cy.get('#link').type(recommendation.link);
 
-    cy.intercept("POST", "/recommendations").as("sendRecommendation");
-    cy.get("button").click();
-    cy.wait("@sendRecommendation");
-    cy.url().should("equal", "http://localhost:3000/");
-  })
+
+describe('test for upvote a recommendation', () => {   
+    it('upvote a recommendation , sucess', () => {
+      const recommendation = {
+        name: faker.random.word(),
+        youtubeLink: `https://www.youtube.com/watch?v=${faker.random.alphaNumeric(11)}`
+      }
   
+      cy.visit("http://localhost:3000/");
+      cy.createRecommendation(recommendation);
+  
+  
+      cy.MultiClick(`[data-cy=${recommendation.name}]`, 10)
+   
+      cy.intercept("POST", "/recommendations/1/upvote").as("upvote");
+      cy.url().should("equal", "http://localhost:3000/");
+    })
+
 })
 
-
 describe('test for upvote a recommendation', () => {
-  it('upvote a recommendation for random', () => {
-    
-    const recommendation = {
-      name: faker.music.songName(),
-      youtubeLink: `https://www.youtube.com/watch?v=${faker.random.alphaNumeric(11)}`
-    }
+  it('create 3 recommendation', () => {
+      const recommendation = {
+          name: faker.random.word(),
+          youtubeLink: `https://www.youtube.com/watch?v=${faker.random.alphaNumeric(11)}`
+      }
+      const recommendation2 = {
+          name: faker.random.word(),
+          youtubeLink: `https://www.youtube.com/watch?v=${faker.random.alphaNumeric(11)}`
+      }
+      const recommendation3 = {
+          name: faker.random.word(),
+          youtubeLink: `https://www.youtube.com/watch?v=${faker.random.alphaNumeric(11)}`
+      }
 
-    cy.visit("http://localhost:3000/");
-    cy.createRecommendation(recommendation);
-    
-    cy.getBySel(recommendation.name,recommendation).click()
-    cy.intercept("POST", "/recommendations/1/upvote").as("upvote");
-    cy.url().should("equal", "http://localhost:3000/");
+
+      cy.visit("http://localhost:3000/");
+
+      cy.createRecommendation(recommendation);
+      cy.createRecommendation(recommendation2);
+      cy.createRecommendation(recommendation3);
+      cy.url().should("equal", "http://localhost:3000/");
   })
 
 })
